f759e6584ebbc03d0912dc02eb3a9378011ba0aa
Eduardo Silva <silvapereira1608gb@gmail.com>
2025-08-15 22:14:57 -0300

    Começando

---
+++ assets/js/popup.js
@@ -0,0 +1,9 @@
+function abrirPopup() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function fecharPopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
